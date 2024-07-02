b725c4047eb72c2480f6e557bf3b3524ff4e0c3a
rednamed <estou.eu.te.amo@gmail.com>
2024-07-03 06:02:03 +0900

    update effects

--- src/components/Portal.tsx
+++ src/components/Portal.tsx
@@ -1,24 +1,31 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import { checkCleared, clearContent, resetCleared } from "../oparateStrage"
 
 export const Portal = () => {
+
+    const [clearingA, setClearingA] = useState(checkCleared("A"))
+    const [clearingB, setClearingB] = useState(checkCleared("B"))
+    const [clearingN, setClearingN] = useState(checkCleared("N"))
+
     useEffect(() => {
         const url = new URL(window.location.href)
         const hash = url.hash;
 
         if (hash.includes("eigogieGaek6lai8")) {
             clearContent("A")
+            setClearingA(true)
         } else if (hash.includes("aish3zaiPhaiwa0U")) {
             clearContent("B")
+            setClearingB(true)
         } else if (hash.includes("Oopeir1iedohrai0")) {
             clearContent("N")
+            setClearingN(true)
         }
     }, [])
-
-    const clearingA = checkCleared("A")
-    const clearingB = checkCleared("B")
-    const clearingN = checkCleared("N")
+    useEffect(() => {
+        console.log({ clearingA, clearingB, clearingN })
+    }, [clearingA, clearingB, clearingN])
 
     return <>
         {clearingA && <div>A くりあずみ</div>}
