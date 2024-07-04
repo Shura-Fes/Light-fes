58ee032bfad15606e0321bdbc92ab6b5db2d9fb0
rednamed <estou.eu.te.amo@gmail.com>
2024-07-05 01:13:26 +0900

    update markdown b

--- src/components/Exp_B.tsx
+++ src/components/Exp_B.tsx
@@ -1,8 +1,10 @@
 import ReactMarkdown from "react-markdown"
 import content from "../assets/markdowns/exp_b.md?raw"
+import water_skelton from "../assets/markdowns/images/water_skelton.avif?url"
+import kenrisho from "../assets/markdowns/images/kenrisho.avif?url"
 
 export const Exp_B = () => {
-    const result = content
+    const result = content.replaceAll("@water_skelton", water_skelton).replaceAll("@kenrisho", kenrisho)
     return <>
         <ReactMarkdown>
             {result}
