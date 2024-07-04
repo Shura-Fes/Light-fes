c4787c1c80cc924b7ab208b213c705439fdd8cec
rednamed <estou.eu.te.amo@gmail.com>
2024-07-04 18:02:09 +0900

    compress images

--- src/components/Exp_B.tsx
+++ src/components/Exp_B.tsx
@@ -2,7 +2,7 @@ import ReactMarkdown from "react-markdown"
 import content from "../assets/markdowns/exp_b.md?raw"
 
 export const Exp_B = () => {
-    const result = content.replace("@image1_url", image1)
+    const result = content
     return <>
         <ReactMarkdown>
             {result}
