d52c25caffc3a214e1d36c879545421cc724b236
rednamed <estou.eu.te.amo@gmail.com>
2024-07-04 09:06:50 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import VitePluginWebpAndPath from "vite-plugin-webp-and-path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePluginWebpAndPath()],
+  plugins: [react(), VitePluginWebpAndPath({textExtensions:"ts,tsx,css,html"})],
 })
