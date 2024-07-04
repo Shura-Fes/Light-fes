88f212b39f0ce6ff2a6cc94923b4ed1028bda1f9
rednamed <estou.eu.te.amo@gmail.com>
2024-07-05 02:00:42 +0900

    update markdown b

--- src/components/Exp_B.tsx
+++ src/components/Exp_B.tsx
@@ -1,7 +1,6 @@
 import ReactMarkdown from "react-markdown"
 import content from "../assets/markdowns/exp_b.md?raw"
 import water_skelton from "../assets/markdowns/images/water_skelton.avif?url"
-import kenrisho from "../assets/markdowns/images/kenrisho.avif?url"
 
 export const Exp_B = () => {
     console.log(water_skelton)
