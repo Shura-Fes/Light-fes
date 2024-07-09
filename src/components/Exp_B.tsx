d93ad7b5f2a75eded7045552cd259a9978a8da89
rednamed <estou.eu.te.amo@gmail.com>
2024-07-09 18:01:38 +0900

    auto image fit

--- src/components/Exp_B.tsx
+++ src/components/Exp_B.tsx
@@ -1,9 +1,6 @@
 import content from "../assets/markdowns/exp_b.md?raw"
-import water_skelton from "../assets/markdowns/images/water_skelton.avif?url"
 import { Markdown } from "./Markdown"
 
 export const Exp_B = () => {
-    console.log(water_skelton)
-    const result = content.replaceAll("@water_skelton", water_skelton)
-    return <Markdown content={result} />
+    return <Markdown content={content} />
 }
--- src/components/Markdown.tsx
+++ src/components/Markdown.tsx
@@ -9,8 +9,27 @@ type Props = {
     content: string
 }
 
-export const Markdown = ({ content }: Props) =>
-    <ReactMarkdown remarkPlugins={[remarkFrontmatter, remarkGfm]}
+export const Markdown = ({ content }: Props) => {
+    const imageReg = /\(@.*\)/g;
+    const images = Array.from(content.matchAll(imageReg)).map(([key]) => key).map(target => target.substring(2, target.length - 1));
+
+    const result = images.reduce((result, image) => {
+        return result.replaceAll(`(@${image})`, `(${getImageUrl(image)})`)
+    }, content)
+
+    return <ReactMarkdown remarkPlugins={[remarkFrontmatter, remarkGfm]}
         rehypePlugins={[rehypeSlug, rehypeHighlight, rehypeRaw]}>
-        {content}
+        {result}
     </ReactMarkdown>
+}
+
+const getImageUrl = (target: string) => {
+    const targetFileType = ["avif", "webp", "jpeg", "jpg", "png"]
+
+    for (const extention of targetFileType) {
+        const url = new URL(`../assets/markdowns/images/${target}.${extention}`, import.meta.url).href
+        if (url.includes(target)) {
+            return url
+        }
+    }
+}
