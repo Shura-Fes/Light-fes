ea59a154ad42731792907038cfedb345950eeccb
rednamed <estou.eu.te.amo@gmail.com>
2024-07-12 15:02:59 +0900

    add banner

--- src/components/Main_KV.tsx
+++ src/components/Main_KV.tsx
@@ -4,10 +4,19 @@ import keyVisualWara from "../assets/keyvisual/syurasai_wara.avif?url"
 import keyVisualColor from "../assets/keyvisual/syurasai_color.avif?url"
 import { Link } from "react-router-dom"
 
+import a_banner from "../assets/banners/a_exp_banner.avif"
+import b_banner from "../assets/banners/b_exp_banner.avif"
+
+import "./Main_KV.css"
+
 
 type Props = {
     clearingA: boolean
     clearingB: boolean
+}
+
+const bannerStyles = {
+    width: "250px"
 }
 
 export const Main_KV = ({ clearingA, clearingB }: Props) => {
@@ -15,13 +24,16 @@ export const Main_KV = ({ clearingA, clearingB }: Props) => {
     return <div style={{ marginBottom: "10px" }}>
         <img src={keyVisual} style={{ objectFit: "contain", maxWidth: "100%" }} />
         <div>
-            {clearingA && <Link to={"ExpA_aiya1iZe"}>参加者サイド解説</Link>}
-            {clearingA && clearingB && "／"}
-            {clearingB && <Link to={"ExpB_Gaiph9Ol"}>制作者サイド解説</Link>}
+            <Link to={clearingA ? "ExpA_aiya1iZe" : ""} className={clearingA ? "" : "disable-link"}>
+                <img src={a_banner} style={bannerStyles} alt="参加者サイド解説" />
+            </Link>
+            <Link to={clearingB ? "ExpB_Gaiph9Ol" : ""} className={clearingB ? "" : "disable-link"}>
+                <img src={b_banner} style={bannerStyles} alt="制作者サイド解説" />
+            </Link>
         </div>
         <div>
-            {clearingA && clearingB && <div style={{ color: "red" }}>２つのサイドをクリアしたので『回らない当日コンテンツ』に参加可能です！</div>}
-            {clearingA && clearingB && <div>参加するには、『回らない当日コンテンツ』を予約して、呼ばれたらこの画面をブースで見せてください！</div>}
+            {clearingA && clearingB && <div style={{ color: "red" }}>２つのサイドをクリアしたので『回らない当日運営』に参加可能です！</div>}
+            {clearingA && clearingB && <div>参加するには、『回らない当日運営』を予約して、呼ばれたらこの画面をブースで見せてください！</div>}
         </div>
     </div>
 }
