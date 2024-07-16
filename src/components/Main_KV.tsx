288bba88ee00da417071f9b80e159d66fa88b762
rednamed <estou.eu.te.amo@gmail.com>
2024-07-16 19:29:42 +0900

    remove flags

--- src/components/Main_KV.tsx
+++ src/components/Main_KV.tsx
@@ -1,6 +1,3 @@
-import keyVisualGray from '../assets/keyvisual/syurasai_gray.avif?url';
-import keyVisualSankasha from '../assets/keyvisual/syurasai_sankasha.avif?url';
-import keyVisualWara from '../assets/keyvisual/syurasai_wara.avif?url';
 import keyVisualColor from '../assets/keyvisual/syurasai_color.avif?url';
 import { Link } from 'react-router-dom';
 
@@ -11,61 +8,45 @@ import { Credits_B } from './Credits_B';
 import ContentCImage from '../assets/keyvisual/syurasai_contents_c.avif?url';
 import { XButton } from './X';
 import { Contents, HashTags, KansoTemplate } from '../Consts';
+import { Credits_C } from './Credits_C';
 
-type Props = {
-  clearingA: boolean;
-  clearingB: boolean;
-};
-
-export const Main_KV = ({ clearingA, clearingB }: Props) => {
-  const keyVisual = getKV(clearingA, clearingB);
+export const Main_KV = () => {
+  const keyVisual = getKV();
   return (
     <div style={{ marginBottom: '10px' }}>
       <img src={keyVisual} />
-      {((clearingA || clearingB) && <div>解説サイトはこちら！</div>)}
+      <div>解説サイトはこちら！</div>
       <div>
-        {
-        clearingA && <Link to={clearingA ? 'ExpA_aiya1iZe' : ''} className={clearingA ? '' : 'disable-link'}>
-          参加者サイド
-        </Link>
-        }
-        {clearingA && clearingB && '／' }
-        {
-        clearingB && <Link to={clearingB ? 'ExpB_Gaiph9Ol' : ''} className={clearingB ? '' : 'disable-link'}>
-          制作者サイド
-        </Link>
-        }
-      </div>
-      <div>{clearingA && <Credits_A />}</div>
-      <div>{clearingB && <Credits_B />}</div>
-      <div>
-        {clearingA && (
-          <XButton title={Contents.A} hashtags={[HashTags.A, HashTags.S, HashTags.L]} message={KansoTemplate} />
-        )}
-        {clearingB && (
-          <XButton title={Contents.B} hashtags={[HashTags.B, HashTags.S, HashTags.L]} message={KansoTemplate} />
-        )}
+        <Link to={'ExpA_aiya1iZe'}>参加者サイド</Link>
+        {'／'}
+        <Link to={'ExpB_Gaiph9Ol'}>制作者サイド</Link>
       </div>
       <div>
-        {clearingA && clearingB && (
-          <>
-            <div style={{ color: 'red', marginBottom: '5px' }}>
-              ２つのサイドをクリアしたので『回らない当日運営からの脱出』に参加可能です！
-            </div>
-            <img src={ContentCImage} />
-            <div>
-              参加するには、『回らない当日運営からの脱出』を予約して、呼ばれたらこの画面をブースで見せてください！
-            </div>
-          </>
-        )}
+        <Credits_A />
+      </div>
+      <div>
+        <Credits_B />
+      </div>
+      <div>
+        <XButton title={Contents.A} hashtags={[HashTags.A, HashTags.S, HashTags.L]} message={KansoTemplate} />
+        <XButton title={Contents.B} hashtags={[HashTags.B, HashTags.S, HashTags.L]} message={KansoTemplate} />
+      </div>
+      <div>
+        <div style={{ color: 'red', marginBottom: '5px' }}>
+          ２つのサイドをクリアすると『回らない当日運営からの脱出』に参加可能でした！
+        </div>
+        <img src={ContentCImage} />
+        <Link to={'Exp3_ouBa4eng'}>
+          <div>修羅祭実行委員会サイドの解説はこちら！</div>
+        </Link>
+      </div>
+      <div>
+        <Credits_C />
       </div>
     </div>
   );
 };
 
-const getKV = (clearingA: boolean, clearingB: boolean) => {
-  if (clearingA && clearingB) return keyVisualColor;
-  if (clearingA) return keyVisualSankasha;
-  if (clearingB) return keyVisualWara;
-  return keyVisualGray;
+const getKV = () => {
+  return keyVisualColor;
 };
--- src/components/Portal.tsx
+++ src/components/Portal.tsx
@@ -1,43 +1,21 @@
-import { useEffect, useState } from "react"
-import { checkCleared, clearContent, resetCleared } from "../oparateStrage"
-import { Main_KV } from "./Main_KV"
-import { Ninetable_KV } from "./Ninetable_KV"
+import { Main_KV } from './Main_KV';
 
 export const Portal = () => {
-
-    const [clearingA, setClearingA] = useState(checkCleared("A"))
-    const [clearingB, setClearingB] = useState(checkCleared("B"))
-    const [clearingN, setClearingN] = useState(checkCleared("N"))
-
-    useEffect(() => {
-        const url = new URL(window.location.href)
-        const code = url.hash ? url.hash.length > 8 ? url.hash.replace("#/?code=", "") : "" : "";
-
-        if (code === ("eigogieGaek6lai8")) {
-            clearContent("A")
-            setClearingA(true)
-        } else if (code === ("aish3zaiPhaiwa0U")) {
-            clearContent("B")
-            setClearingB(true)
-        } else if (code === ("Oopeir1iedohrai0")) {
-            clearContent("N")
-            setClearingN(true)
-        } else if (code === ("DieV8yiiPahtooqu")) {
-            resetCleared()
-            setClearingA(false)
-            setClearingB(false)
-            setClearingN(false)
-        }
-    }, [])
-    useEffect(() => {
-        console.log({ clearingA, clearingB, clearingN })
-    }, [clearingA, clearingB, clearingN])
-
-    return <>
-        <Main_KV
-            clearingA={clearingA}
-            clearingB={clearingB}
-        />
-        <Ninetable_KV clearingN={clearingN} />
-    </>
-}
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <Main_KV />
+      <div
+        style={{
+          border: '2px solid #666',
+          margin: '2px',
+          padding: '10px',
+          backgroundColor: 'lightblue',
+          color: 'darkviolet',
+          width: '100%',
+        }}
+      >
+        推奨環境：Google Chrome
+      </div>
+    </div>
+  );
+};
